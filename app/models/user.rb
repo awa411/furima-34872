b2b417a798b91827433600adffee5f2b4b8fb9fb
awa411 <ad1117wi@gmail.com>
2021-03-13 10:53:20 +0900

    fix commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
-  validates :nickname,           presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_kana,     presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,           presence: true
-  validates_format_of :password, with: PASSWORD_REGEX
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
+  validates_format_of :password,          with: PASSWORD_REGEX
   validates_format_of :last_name,         with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   validates_format_of :first_name,        with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   validates_format_of :last_name_kana,    with: /\A[ァ-ヶ]+\z/
