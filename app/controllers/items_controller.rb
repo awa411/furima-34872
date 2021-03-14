a2bec2d53a267e67c51bc089dfb7458c33c635b4
awa411 <ad1117wi@gmail.com>
2021-03-14 14:40:33 +0900

    price_tax ~ error_messages

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :shipping_charge_id, :state_id, :day_to_ship_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item < ApplicationRecord
-  with options presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :shipping_charge
+    belongs_to :state
+    belongs_to :day_to_ship
+
+  with_options presence: true
     validates :item_name
     validates :description
+    validates :price, format: { with: /\A[300-9999999]+\z/ }
+
+  with_options presence: true, numericality: { other_than: 1 }
     validates :category_id
     validates :status_id
     validates :shipping_charge_id
     validates :state_id
     validates :day_to_ship_id
-    validates :price, format: { with: /\A[300-9999999]+\z/ }
-
 end
