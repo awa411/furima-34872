2bfcc2a593b44074e0f683ab212e8a52a11ce75e
awa411 <ad1117wi@gmail.com>
2021-03-18 12:06:12 +0900

    fix3 commit

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     building     { 'あ棟'}
     phone_number { '00000000000' }
     token        {"tok_abcdefghijk00000000000000000"}
-    association :item
-    association :user
   end
 end
