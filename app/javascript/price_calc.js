c20844e9e733dc74f29883065d443528105dc8e6
awa411 <ad1117wi@gmail.com>
2021-03-23 12:36:49 +0900

    preview commit

--- app/javascript/price_calc.js
+++ app/javascript/price_calc.js
@@ -1,19 +1,21 @@
-function calc (){
-  const item_price = document.getElementById("item-price");
-    item_price.addEventListener('input', function (){
-      const price_value = item_price.value;
-      const add_tax_price = document.getElementById("add-tax-price");
-      const profit = document.getElementById("profit");
-      function calc_tax (p) {
-        return p * 0.1;
-      } 
-      add_tax_price.innerHTML = `${calc_tax(price_value)}`;
-      profit.innerHTML = `${price_value - calc_tax(price_value)}`;
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  function calc (){
+    const item_price = document.getElementById("item-price");
+      item_price.addEventListener('input', function (){
+        const price_value = item_price.value;
+        const add_tax_price = document.getElementById("add-tax-price");
+        const profit = document.getElementById("profit");
+        function calc_tax (p) {
+          return p * 0.1;
+        } 
+        add_tax_price.innerHTML = `${calc_tax(price_value)}`;
+        profit.innerHTML = `${price_value - calc_tax(price_value)}`;
+      
+      } )
+
     
-    } )
+    
+  };
 
-  
-  
-};
-
-window.addEventListener('load', calc);
+  window.addEventListener('load', calc);
+}
