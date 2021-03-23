e8c64b1439ec4b3c6b7cab60987dcd1b19448fb3
awa411 <ad1117wi@gmail.com>
2021-03-23 22:15:12 +0900

    previews2

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,6 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
+    const imageList = document.getElementById('image-list');
    
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
@@ -8,28 +8,42 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       let imageElementNum = document.querySelectorAll('.image-element').length;
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
-      const inputHTML = document.createElement('input');
-      inputHTML.setAttribute('id', `message_image_${imageElementNum}`);
-      inputHTML.setAttribute('name', 'message[images][]');
-      inputHTML.setAttribute('type', 'file');
-      blobImage.setAttribute('class', 'image-content');
-      imageElement.appendChild(blobImage);
-      imageElement.appendChild(inputHTML);
-      ImageList.appendChild(imageElement);
+      blobImage.setAttribute('id', `image-content[${imageElementNum}]`)
+      
 
-      inputHTML.addEventListener('change', (e) => {
-        file = e.target.files[0];
-        blob = window.URL.createObjectURL(file);
+      // const inputHTML = document.createElement('input');
+      // inputHTML.setAttribute('id', `message_image_${imageElementNum}`);
+      // inputHTML.setAttribute('name', 'message[images][]');
+      // inputHTML.setAttribute('type', 'file');
+      // inputHTML.setAttribute('class', 'message_image');
+      // inputHTML.setAttribute('style', 'display: none;');
+       blobImage.setAttribute('class', `image-content`);
 
-        createImageHTML(blob);
-      })
-    }
+       imageElement.appendChild(blobImage);
+      // imageElement.appendChild(inputHTML);
+       imageList.appendChild(imageElement);
+
+      // inputHTML.addEventListener('change', (e) => {
+          
+      //   file = e.target.files[0];
+      //   blob = window.URL.createObjectURL(file);
+
+      //   createImageHTML(blob);
+      // });
+    };
    
     document.getElementById('item-image').addEventListener('change', function(e){
-      // const imageContent = document.querySelector('img');
-      // if (imageContent){
-      //   imageContent.remove();
-      // }
+         const imageContent = document.querySelector('img');
+         if (imageContent){
+           const  clickUpLoad = document.getElementById("click-upload");
+           const  imgUpLoad = document.getElementById("img-upload");
+           const  imageList = document.getElementById('image-list');
+           const  upLoadMessage = document.getElementById('upload-message');
+           let imageElementNum = document.querySelectorAll('.image-element').length;
+           imgUpLoad.insertBefore(clickUpLoad, imageList);
+           upLoadMessage.innerHTML = `クリックして${imageElementNum+2}枚目の画像を追加できます`
+
+         };
       
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
